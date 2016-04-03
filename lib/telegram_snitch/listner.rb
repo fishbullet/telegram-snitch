374ae300147b7572bad4959834105f01edeaa7db
fishbullet <shindu666@gmail.com>
2016-04-03 18:55:19 +0000

    add request host

--- lib/telegram_snitch/listner.rb
+++ lib/telegram_snitch/listner.rb
@@ -6,6 +6,8 @@ module TelegramSnitch
   class Listner < Sinatra::Base
     post '/listen' do
       json = MultiJson.load(request.body.read, symbolize_keys: true)
+      Base.logger.info("Receive request from: #{request.host}")
+
       Base.logger.info("Listner -- receive: #{json}")
 
       Base.redis.publish("activity", json[:message])
